5450f689abce397c780ed606d645d15dc1ae0ea8
Sebastian Korfmann <sebastian@korfmann.net>
2020-08-17 14:08:51 +0200

    Initial import

---
+++ .projenrc.js
@@ -0,0 +1,8 @@
+const { CdktfProviderProject } = require('@cdktf/provider-project');
+const { Semver } = require('projen');
+
+const project = new CdktfProviderProject({
+  terraformProvider: "terraform-providers/docker@~> 2.0"
+});
+
+project.synth();
