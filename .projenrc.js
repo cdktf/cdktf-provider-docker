025f60df4b3344b3434e44ac55dcdae3bcc2b8c5
Sebastian Korfmann <sebastian@korfmann.net>
2021-01-29 23:44:56 +0100

    Upgrade provider project

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,8 @@ const { CdktfProviderProject } = require('@cdktf/provider-project');
 const { Semver } = require('projen');
 
 const project = new CdktfProviderProject({
-  terraformProvider: "terraform-providers/docker@~> 2.0"
+  terraformProvider: 'terraform-providers/docker@~> 2.0',
+  eslint: false,
 });
 
 project.synth();
