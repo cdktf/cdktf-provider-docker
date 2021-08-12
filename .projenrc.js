a93b56f9e63fb28c618f03dd1ddd26bb37da6490
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2021-08-12 21:55:54 +0000

    chore: upgrade provider-project and projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,6 @@
 const { CdktfProviderProject } = require('@cdktf/provider-project');
-const { Semver } = require('projen');
-
 const project = new CdktfProviderProject({
-  terraformProvider: 'kreuzwerker/docker@~> 2.12',
-  eslint: false,
+  terraformProvider: "kreuzwerker/docker@~> 2.12"
 });
 
 project.synth();
