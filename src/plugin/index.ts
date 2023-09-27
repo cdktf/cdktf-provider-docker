b920adc1f74725b637f1149b8478e4a4d92b6bfd
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-27 08:25:47 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/kreuzwerker/docker/3.0.2/docs/resources/plugin
 // generated from terraform resource schema
 
